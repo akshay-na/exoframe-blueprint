6e0486707298a563a4173a6e9480355a3d91a9ae
akshay-na <akshayna04@gmail.com>
2025-04-16 11:12:19 +0530

    chore: finalise decorators

--- src/lib/decorators/RouteDecorators.ts
+++ src/lib/decorators/RouteDecorators.ts
@@ -4,41 +4,70 @@ import { RouteOptions } from "./types";
 /**
  * Declares the base path and version for the route.
  */
-export function Route(path: string, options: RouteOptions): ClassDecorator {
-  return (target: Function) => {
+export function Route(path: string, options?: { version?: string }) {
+  return function (target: any) {
     Reflect.defineMetadata("route:path", path, target);
-    Reflect.defineMetadata("route:version", options.version, target);
+    if (options?.version) {
+      Reflect.defineMetadata("route:version", options.version, target);
+    }
+  };
+}
+
+export function RouteDescription(description: string) {
+  return function (target: any) {
+    Reflect.defineMetadata("route:description", description, target);
   };
 }
 
 /**
  * Marks the route as discoverable by a router with a matching tag.
  */
-export function Discoverable(tag: string): ClassDecorator {
-  return (target: Function) => {
-    Reflect.defineMetadata("route:tag", tag, target);
+export function Discoverable(routeId: string) {
+  return function (target: any) {
+    Reflect.defineMetadata("route:discoverable", routeId, target);
   };
 }
 
 /**
  * Specifies the HTTP method for this endpoint.
  */
-export function Endpoint(method: string): MethodDecorator {
-  return (target, propertyKey, descriptor) => {
-    Reflect.defineMetadata(
-      "route:endpoint",
-      method.toLowerCase(),
-      descriptor.value!
-    );
+export function Endpoint(
+  method: "GET" | "POST" | "PUT" | "DELETE",
+  options?: { hints?: string[] }
+) {
+  return function (target: any, propertyKey: string) {
+    Reflect.defineMetadata("endpoint:method", method, target, propertyKey);
+    if (options?.hints) {
+      Reflect.defineMetadata(
+        "endpoint:hints",
+        options.hints,
+        target,
+        propertyKey
+      );
+    }
   };
 }
 
 /**
  * Maps the arguments from the request to the endpoint method.
  */
-export function ArgumentMapping(mapping: string[]): MethodDecorator {
-  return (target, propertyKey, descriptor) => {
-    Reflect.defineMetadata("route:argumentMapping", mapping, descriptor.value!);
+export function ArgumentMapping(mapping: Array<any>) {
+  return function (target: any, propertyKey: string) {
+    Reflect.defineMetadata("endpoint:arguments", mapping, target, propertyKey);
+  };
+}
+
+export function Configuration(options: {
+  access: string;
+  guard: { rights: string };
+}) {
+  return function (target: any, propertyKey: string) {
+    Reflect.defineMetadata(
+      "endpoint:configuration",
+      options,
+      target,
+      propertyKey
+    );
   };
 }
 
